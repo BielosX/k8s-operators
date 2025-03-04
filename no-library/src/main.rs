20096eee93c0ffa09b3a0040443eeba30ad22f62
Mateusz Biel <matbiel@gmail.com>
2025-03-04 11:21:28 +0100

    Add health endpoint

--- no-library/src/main.rs
+++ no-library/src/main.rs
@@ -1,19 +1,28 @@
+use axum::routing::get;
+use axum::Router;
+use tokio::select;
 use tokio::time::{sleep, Duration};
 use tracing::info;
 
 async fn get_ip() -> reqwest::Result<String> {
-    reqwest::get("https://api.ipify.org")
-        .await?
-        .text()
-        .await
+    reqwest::get("https://api.ipify.org").await?.text().await
+}
+
+async fn operator() {
+    loop {
+        let ip = get_ip().await.unwrap();
+        info!("Public IP: {}", ip);
+        sleep(Duration::from_secs(10)).await;
+    }
 }
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
-    loop {
-        let ip = get_ip().await.unwrap();
-        info!("Public IP: {}", ip);
-        sleep(Duration::from_secs(10)).await;
+    let app = Router::new().route("/healthz", get(async || "OK"));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    select! {
+        _ = tokio::spawn(operator()) => {}
+        _ = axum::serve(listener, app) => {}
     }
 }
