b279cb75aa9e84cb9f0b4074166cde9fab640dc6
Mateusz Biel <matbiel@gmail.com>
2025-03-05 01:10:00 +0100

    Refactor main.rs

--- no-library/src/main.rs
+++ no-library/src/main.rs
@@ -1,38 +1,49 @@
 use axum::routing::get;
 use axum::Router;
-use std::env;
 use reqwest::{Certificate, Client};
+use std::env;
+use tokio::fs;
 use tokio::select;
 use tokio::time::{sleep, Duration};
 use tracing::info;
-use tokio::fs;
 
 const SERVICE_ACCOUNT_PATH: &str = "/var/run/secrets/kubernetes.io/serviceaccount";
 const API_SERVER: &str = "https://kubernetes.default.svc";
 const EXPOSED_APPS_LIST: &str = "/apis/stable.no-library.com/v1/exposedapps";
 
-async fn get_token() -> Result<String, String> {
+async fn get_token() -> String {
     let content = fs::read(format!("{}/token", SERVICE_ACCOUNT_PATH))
-        .await.map_err(|err| err.to_string())?;
-    String::from_utf8(content).map_err(|_err| { "Unable to load token".to_string() })
+        .await
+        .expect("Unable to read token");
+    String::from_utf8(content).expect("Unable to convert token to UTF8")
 }
 
-async fn get_client() -> Result<Client, String> {
+async fn get_client() -> Client {
     let cert = fs::read(format!("{}/ca.crt", SERVICE_ACCOUNT_PATH))
-        .await.map_err(|err| err.to_string())?;
-    let certificate = Certificate::from_pem(cert.as_slice()).map_err(|err| err.to_string())?;
-    Client::builder().add_root_certificate(certificate).build().map_err(|err| err.to_string())
+        .await
+        .expect("Unable to read certificate");
+    let certificate = Certificate::from_pem(cert.as_slice()).expect("Unable to create Certificate");
+    Client::builder()
+        .add_root_certificate(certificate)
+        .build()
+        .expect("Unable to add root certificate")
 }
 
 async fn operator() {
-    let client = get_client().await.unwrap();
+    let client = get_client().await;
     loop {
-        let token = get_token().await.unwrap();
+        let token = get_token().await;
         info!("Token fetched");
-        let request = client.get(format!("{}{}", API_SERVER, EXPOSED_APPS_LIST))
+        let request = client
+            .get(format!("{}{}", API_SERVER, EXPOSED_APPS_LIST))
             .header("Authorization", format!("Bearer {}", token))
-            .build().unwrap();
-        let response = client.execute(request).await.map_err(|err| err.to_string()).unwrap();
+            .build()
+            .unwrap();
+        let response = client
+            .execute(request)
+            .await
+            .map_err(|err| err.to_string())
+            .unwrap();
         info!("Received: {}", response.text().await.unwrap());
         sleep(Duration::from_secs(10)).await;
     }
