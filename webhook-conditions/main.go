c13f0d1dbcf65cc6eb69ca6b9dd79cfbe57bd697
Mateusz Biel <matbiel@gmail.com>
2025-04-21 20:53:37 +0200

    Implement metrics

--- webhook-conditions/main.go
+++ webhook-conditions/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	admissionv1 "k8s.io/api/admission/v1"
 	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -20,7 +23,7 @@ type statusAwareResponseWriter struct {
 
 func (writer *statusAwareResponseWriter) WriteHeader(statusCode int) {
 	writer.statusCode = statusCode
-	writer.WriteHeader(statusCode)
+	writer.ResponseWriter.WriteHeader(statusCode)
 }
 
 func handleFunc(pattern string, handler func(http.ResponseWriter, *http.Request)) {
@@ -32,47 +35,80 @@ func handleFunc(pattern string, handler func(http.ResponseWriter, *http.Request)
 	})
 }
 
-func validate(w http.ResponseWriter, r *http.Request) {
-	review := admissionv1.AdmissionReview{}
-	err := json.NewDecoder(r.Body).Decode(&review)
-	if err != nil {
-		slog.Error("Unable to parse AdmissionReview", "error", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	slog.Info("Received AdmissionRequest", "uid", review.Request.UID)
+func handle(pattern string, handler http.Handler) {
+	handleFunc(pattern, func(w http.ResponseWriter, r *http.Request) {
+		handler.ServeHTTP(w, r)
+	})
+}
+
+func validate(request *admissionv1.AdmissionRequest) (*admissionv1.AdmissionResponse, error) {
+	slog.Info("Received AdmissionRequest", "uid", request.UID)
 	oldDeployment := &appsv1.Deployment{}
-	err = json.Unmarshal(review.Request.OldObject.Raw, oldDeployment)
+	err := json.Unmarshal(request.OldObject.Raw, oldDeployment)
 	if err != nil {
 		slog.Error("Unable to parse Old Deployment", "error", err)
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		return nil, err
 	}
-	review.Response = &admissionv1.AdmissionResponse{
+	response := &admissionv1.AdmissionResponse{
 		Allowed: true,
-		UID:     review.Request.UID,
+		UID:     request.UID,
 	}
 	if oldDeployment.Annotations != nil {
 		value, ok := oldDeployment.Annotations["immutable"]
 		if ok && value == "true" {
 			slog.Info("Deployment marked as immutable")
-			review.Response.Allowed = false
-			review.Response.Result = &metav1.Status{
+			response.Allowed = false
+			response.Result = &metav1.Status{
 				Code:    http.StatusBadRequest,
 				Message: "Deployment marked as Immutable, unable to Update. Delete first, then recreate",
 			}
 		}
 	}
+	return response, nil
+}
+
+func validateHandler(w http.ResponseWriter, r *http.Request) {
+	review := admissionv1.AdmissionReview{}
+	err := json.NewDecoder(r.Body).Decode(&review)
+	if err != nil {
+		slog.Error("Unable to parse AdmissionReview", "error", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		serverErrors.Inc()
+		return
+	}
+	if review.Request == nil {
+		slog.Error("Request field not found in AdmissionReview")
+		w.WriteHeader(http.StatusInternalServerError)
+		serverErrors.Inc()
+		return
+	}
+	requests.Inc()
+	response, err := validate(review.Request)
+	if err != nil {
+		slog.Error("Validate failed with error", "error", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		serverErrors.Inc()
+		return
+	}
+	review.Request = nil
+	review.Response = response
+	if response.Allowed {
+		allowed.Inc()
+	} else {
+		denied.Inc()
+	}
 	payload, err := json.Marshal(review)
 	if err != nil {
 		slog.Error("Unable to serialize AdmissionReview", "error", err)
 		w.WriteHeader(http.StatusInternalServerError)
+		serverErrors.Inc()
 		return
 	}
 	_, err = w.Write(payload)
 	if err != nil {
 		slog.Error("Unable to send response", "error", err)
 		w.WriteHeader(http.StatusInternalServerError)
+		serverErrors.Inc()
 	}
 }
 
@@ -83,6 +119,25 @@ func health(writer http.ResponseWriter, _ *http.Request) {
 	}
 }
 
+var (
+	serverErrors = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "webhook_conditions_validate_internal_server_error_total",
+		Help: "The total number of 5xx returned by /validate",
+	})
+	allowed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "webhook_conditions_allowed",
+		Help: "The total number of allowed AdmissionRequests",
+	})
+	denied = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "webhook_conditions_denied",
+		Help: "The total number of denied AdmissionRequests",
+	})
+	requests = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "webhook_conditions_validate_requests",
+		Help: "The total number of all successfully parsed AdmissionRequests",
+	})
+)
+
 func main() {
 	port, err := strconv.ParseUint(os.Getenv("PORT"), 10, 16)
 	if err != nil {
@@ -91,8 +146,9 @@ func main() {
 	certFile := os.Getenv("CERT_FILE")
 	keyFile := os.Getenv("KEY_FILE")
 
-	handleFunc("/validate", validate)
+	handleFunc("/validate", validateHandler)
 	handleFunc("/healthz", health)
+	handle("/metrics", promhttp.Handler())
 
 	listener, err := net.Listen("tcp", fmt.Sprintf("0.0.0.0:%d", port))
 	if err == nil {
